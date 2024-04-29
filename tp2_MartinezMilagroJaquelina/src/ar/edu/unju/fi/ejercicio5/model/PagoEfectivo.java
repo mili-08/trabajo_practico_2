b92bafeb5fbc58cfe528dc33a208b216048cb595
mili-08 <milijmartinez@gmail.com>
2024-04-29 18:59:33 -0300

    Ejercicio 5: Creacion de la rama feature/ejercicio5, paquetes y clases

---
+++ tp2_MartinezMilagroJaquelina/src/ar/edu/unju/fi/ejercicio5/model/PagoEfectivo.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio5.model;
+
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+
+public class PagoEfectivo implements Pago {
+
+	@Override
+	public void realizarPago(double monto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void imprimirRecibo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
